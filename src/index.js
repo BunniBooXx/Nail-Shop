88375db467bd0ae9b9eca68dcf10368c90139bce
BunniBooXx <140341233+BunniBooXx@users.noreply.github.com>
2024-06-30 16:14:32 -0400

    updating 123

--- src/index.js
+++ src/index.js
@@ -4,20 +4,6 @@ import './index.css';
 import App from './App';
 import './input.css';
 import { AuthProvider } from './AuthContext';
-import helmet from 'helmet'; // Import the helmet library
-
-const csp = helmet.contentSecurityPolicy({
-  directives: {
-    connectSrc: [
-      "'self'",
-      'https://api.stripe.com',
-      'https://errors.stripe.com',
-      'https://r.stripe.com',
-      'https://ppm.stripe.com',
-      'https://merchant-ui-api.stripe.com', // Add this line
-    ],
-  },
-});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -28,8 +14,6 @@ root.render(
   </React.StrictMode>
 );
 
-// Apply the CSP middleware
-csp.apply();
 
 
 
