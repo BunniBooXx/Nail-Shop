f91b23ff6fb386d8f7d720caf184a43897682c3e
BunniBooXx <140341233+BunniBooXx@users.noreply.github.com>
2024-06-29 21:58:27 -0400

    debugging 1234

--- src/OrderSuccessPage.jsx
+++ src/OrderSuccessPage.jsx
@@ -36,15 +36,15 @@ const OrderSuccessPage = () => {
           <div className="box">
             <h1>Order Success</h1>
           </div>
-          <br></br>
+          <br />
           <div className="box">
             <p>Your order with ID {order.order_id} has been successfully placed.</p>
           </div>
-          <br></br>
+          <br />
           <div className="box">
             <p>Total Amount: ${order.total_amount}.00</p>
           </div>
-          <br></br>
+          <br />
           <div className="box">
             <p className="intro">You will be getting a confirmation email. Keep in mind orders are handmade and take 2-3 weeks to fulfill. You will get an email with the shipment link when your order is shipped out.</p>
           </div>
@@ -57,5 +57,6 @@ const OrderSuccessPage = () => {
 };
 
 export default OrderSuccessPage;
+
 
 
