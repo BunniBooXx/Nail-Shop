cba20bd481e2f40cba7d560f00f3f23b470ad85e
BunniBooXx <140341233+BunniBooXx@users.noreply.github.com>
2024-08-03 21:13:35 -0400

    updating css

--- src/OrderSuccessPage.jsx
+++ src/OrderSuccessPage.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
-import './App.css'; // Ensure this is the correct path to your CSS file
+import './OrderSuccessPage.css'; // Ensure this is the correct path to your CSS file
 
 const OrderSuccessPage = () => {
   const { orderId } = useParams();
